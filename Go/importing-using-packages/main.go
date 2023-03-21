1659a0c58a7db7502ee720f21e6fec3262e5257c
Spectraza <savychvp@gmail.com>
2023-03-21 03:16:45 +0200

    changed how you said

--- Go/importing-using-packages/main.go
+++ Go/importing-using-packages/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	a := rand.Intn(100)
-	b := math.Sqrt(float64(a))
-	fmt.Println(b)
+	rannum := rand.Intn(100)
+	sqrt := math.Sqrt(float64(rannum))
+	fmt.Printf("Sqrt of %d is %.1f", rannum, sqrt)
 }
