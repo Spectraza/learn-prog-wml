57aa1c5e74104647ac53480dc170c84c3195c9cb
Spectraza <savychvp@gmail.com>
2023-03-22 21:06:59 +0200

    divide func done

--- Go/functions/main.go
+++ Go/functions/main.go
@@ -1,15 +1,15 @@
 package main
 
-import "fmt"
-
 func add(x, y int) int {
 	return x + y
 }
 
-func divine(x, y float64) float64 {
-	return x / y
+func divide(x, y int) (int, int) {
+	quotient := x / y
+	remainder := x % y
+	return quotient, remainder
 }
 
 func main() {
-	fmt.Println(add(2, 2), divine(24, 2))
+
 }
